0653c5ef4f80bf6a942bc43f0e1f1fb7fdc9e1cc
Beytullahacar <119817531+Beytullahacar@users.noreply.github.com>
2023-02-18 19:11:39 +0300

    Add files via upload

---
+++ HesapMakinesi/Main.java
@@ -0,0 +1,39 @@
+package HesapMakinesi;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n1,n2,select;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("İlk sayıyı giriniz : ");
+        n1 = scan.nextInt();
+        System.out.print("İkinci sayıyı giriniz : ");
+        n2 = scan.nextInt();
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz : ");
+        select = scan.nextInt();
+
+        switch (select){
+            case 1 :
+                System.out.println("Toplam : " + (n1 + n2));
+                break;
+            case 2 :
+                System.out.println("Çıkarma : " + (n1 - n2));
+                break;
+            case 3 :
+                System.out.println("Çarpma : " + (n1 * n2));
+                break;
+            case 4 :
+                if (n2 != 0) {
+                    System.out.println("Bölüm : " + (n1 / n2));
+                }else {
+                    System.out.println("Bölen değer 0 olamaz!");
+                }
+                break;
+            default:
+                System.out.println("Yanlış seçim yaptınız tekrar deneyiniz!");
+        }
+    }
+}
